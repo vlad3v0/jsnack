26f0c45cbff43479b73952d491c7c179cf522c4d
vladevgeorge <vbt.george@gmail.com>
2020-04-23 11:51:24 +0200

    snack 1-3

---
+++ main.js
@@ -0,0 +1,71 @@
+/*00000000000000000000000000000000000000000000000
+000000000000 JSnack Exercise 00000000000000000000
+000000000000000000000000000000000000000000000000*/
+
+/* 1-1 - Chiedi all’utente il cognome, inseriscilo in un
+array con altri cognomi e stampa la lista
+ordinata alfabeticamente. Scrivi a che
+posizione della lista il nuovo utente si trova */
+
+
+// let surname = ['rossi', 'bianchi', 'mare'];
+
+// console.log(surname);
+
+// let surnameUser = prompt('Inserisci il tuo cognome!');
+
+// console.log(surname);
+
+
+// function insertSurname(surnameUser) {
+
+//     if (surnameUser != ('' || 'surname') ) {
+
+//         surname.push(surnameUser)
+
+//         orderSurname = surname.sort();
+
+//         console.log(surname);
+        
+//     } else {
+//         console.log('Il cognome non è valido');
+//     };
+
+//     return
+// }
+
+// function showSurname() {
+//     for (let a = 0; a < surname.length; a++) {
+
+//         b = '<li> '  + surname[a] + ' ' + surname.indexOf(surname[a]) + ' </li>';
+//         document.write(b);
+//      };
+
+//      return;
+// };
+
+// insertSurname(surnameUser);
+// showSurname();
+
+
+/* 1-3 Il software deve chiedere per 5 volte all’utente di inserire un
+numero. Il programma stampa la somma di tutti i numeri
+inseriti. Esegui questo programma in due versioni, con il for
+e con il while */
+
+// let numberUser = 0;
+// let number = [];
+// let somma = 0;
+
+// for (let a = 0; a < 5; a++) {
+
+//     numberUser = +prompt('Number?');
+
+//     number.push(numberUser);
+
+//     somma += number[a];
+
+// }
+
+// console.log(number);
+// console.log(somma);
